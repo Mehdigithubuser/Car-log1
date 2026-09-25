473d3d1f35420ca9ac9740ee8ac11604920e936c
Mehdigithubuser <102742843+Mehdigithubuser@users.noreply.github.com>
2026-09-25 11:11:53 +0330

    Add CarLog V1 build configuration

---
+++ app/build.gradle.kts
@@ -0,0 +1,17 @@
+plugins {
+    id("com.android.application")
+}
+android {
+    namespace = "com.mehdi.carlog"
+    compileSdk = 36
+    defaultConfig {
+        applicationId = "com.mehdi.carlog"
+        minSdk = 26
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0.0"
+    }
+    buildTypes {
+        release { minifyEnabled = false }
+    }
+}
